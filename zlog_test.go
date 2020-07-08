a462e6597ef7a10bc54797556da5b32c05ec2a5d
zongran <shuzongran@163.com>
2020-07-08 18:19:59 +0800

    no message

--- zlog_test.go
+++ zlog_test.go
@@ -4,6 +4,30 @@ import (
 	"testing"
 )
 
-func TestZLog(*testing.T) {
-	logger.Debug("hello", Field("Sex", "Girl"))
+func TestDebug(*testing.T) {
+	Debug("Debug", Field("Test", "test Debug"))
+}
+
+func TestInfo(*testing.T) {
+	Info("Info", Field("Test", "test Info"))
+}
+
+func TestWarn(*testing.T) {
+	Warn("Warn", Field("Test", "test Warn"))
+}
+
+func TestError(*testing.T) {
+	Error("Error", Field("Test", "test Error"))
+}
+
+func TestDPanic(*testing.T) {
+	DPanic("DPanic", Field("Test", "test DPanic"))
+}
+
+func TestFatal(*testing.T) {
+	Fatal("Fatal", Field("Test", "test Fatal"))
+}
+
+func TestPanic(*testing.T) {
+	Panic("Panic", Field("Test", "test Panic"))
 }
