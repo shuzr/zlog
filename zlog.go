23d4cff83c745bc739480a337a39ef20684475d4
zongran <shuzongran@163.com>
2020-07-08 18:01:18 +0800

    no message

--- zlog.go
+++ zlog.go
@@ -58,54 +58,26 @@ func Debug(template string, fields ...zap.Field) {
 	logger.Debug(template, fields...)
 }
 
-// func Debugf(template string, args ...interface{}) {
-// 	sugarLogger.Debugf(template, args...)
-// }
-
 func Info(template string, fields ...zap.Field) {
 	logger.Info(template, fields...)
 }
-
-// func Infof(template string, args ...interface{}) {
-// 	sugarLogger.Infof(template, args...)
-// }
 
 func Warn(template string, fields ...zap.Field) {
 	logger.Warn(template, fields...)
 }
 
-// func Warnf(template string, args ...interface{}) {
-// 	sugarLogger.Warnf(template, args...)
-// }
-
 func Error(template string, fields ...zap.Field) {
 	logger.Error(template, fields...)
 }
-
-// func Errorf(template string, args ...interface{}) {
-// 	sugarLogger.Errorf(template, args...)
-// }
 
 func DPanic(template string, fields ...zap.Field) {
 	logger.DPanic(template, fields...)
 }
 
-// func DPanicf(template string, args ...interface{}) {
-// 	sugarLogger.DPanicf(template, args...)
-// }
-
 func Panic(template string, fields ...zap.Field) {
 	logger.Panic(template, fields...)
 }
 
-// func Panicf(template string, args ...interface{}) {
-// 	sugarLogger.Panicf(template, args...)
-// }
-
 func Fatal(template string, fields ...zap.Field) {
 	logger.Fatal(template, fields...)
 }
-
-// func Fatalf(template string, args ...interface{}) {
-// 	sugarLogger.Fatalf(template, args...)
-// }
